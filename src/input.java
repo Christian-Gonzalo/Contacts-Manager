cd12d424195f6f1f65eb97c2816987b113654699
Christian Parker <christianparker512@gmail.com>
2020-12-19 18:52:07 -0600

    Add the try/catch in a do-while loop in regards to the input and whether it is or isn't an integer

--- src/input.java
+++ src/input.java
@@ -14,3 +14,22 @@ public boolean yesNO(){
     char checkValue = input.charAt(0);
     return checkValue == "y";
 }
+public int getInt(int min, int max){
+    int input;
+    boolean exceptionFound;
+    boolean outOfBounds = false;
+    do {
+        exceptionFound = false;
+        System.out.println("Hello. Enter a number between " + min + " and " + max + ": ");
+        outOfBounds = true;
+    } else {
+        // user entered a correct integer
+        outOfBounds = false;
+    } catch ( Exception e) {
+        System.out.println("Hey, this is not an integer");
+        System.out.println();
+        exceptionFound = true;
+    }
+    } while (exceptionFound || outOfBounds);
+            return input;
+        }
