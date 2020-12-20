6e0c1a1a37136e229af736b1f7dc22ba6c12e241
Christian Parker <christianparker512@gmail.com>
2020-12-19 18:44:15 -0600

    start of the input

--- src/input.java
+++ src/input.java
@@ -1,2 +1,16 @@
-public class input {
+public class Input {
+    private final Scanner scanner;
+}
+//** this is the scanner for input the menu.
+public Input () {
+    this.scanner = Scanner(System.in);
+}
+public String get(){
+    return this.scanner.nextLine();
+}
+public boolean yesNO(){
+    System.out.println(("Yes(y) or No(n)"));
+    String input = getString().trim().toLowerCase();
+    char checkValue = input.charAt(0);
+    return checkValue == "y";
 }
